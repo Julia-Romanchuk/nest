e1868233407abefe0b42558b53950a7153bc0b86
julia_cs <romanchukyulia@gmail.com>
2020-09-24 02:29:50 +0300

    add pipes config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()) // force entire app use validation
+  // force entire app use validation
+  app.useGlobalPipes(new ValidationPipe({
+    // remove all properties extra, unneeded fields, leave only described in DTO
+    whitelist: true,
+    // throw an error if any unneeded fields was received and told what cause problem
+    forbidNonWhitelisted: true,
+    // transform all incoming obj to a DTO objects
+    transform: true
+  }))
   await app.listen(3000);
 }
 bootstrap();
